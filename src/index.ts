33048eab2d199db2e78c4201e3d4e6fdfd251ca1
Kangming Deng <dengkangming@gmail.com>
2022-10-06 03:08:50 -0400

    Fix service worker registration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import './piano/index';
 
-if (navigator.serviceWorker) {
-	// Prevent Parcel from resolving this URL.
-	const url = new (class extends URL { })('./service-worker.js', import.meta.url);
-
-	// Prevent service worker from registering during development.
-	if (url.protocol !== 'file:') {
-		navigator.serviceWorker.register(url);
-	}
-}
+navigator.serviceWorker?.register('./service-worker.js');
